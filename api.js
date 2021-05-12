7f0f6ca08b18e5bdf2d800d5317362cf94f2b99e
Jason smith <jasonsmith@Jasons-MacBook-Pro.local>
2021-05-12 17:17:50 -0400

    final draft for now

--- api.js
+++ api.js
@@ -1,10 +1,28 @@
-const baseURL = 'https://cat-fact.herokuapp.com/facts'
+const baseURL = 'https://cat-fact.herokuapp.com/facts/random'
+let button = document.querySelector('button');
+button.addEventListener('click', fetchresults);
 
-
-fetch('https://cat-fact.herokuapp.com/facts')
+function fetchresults() {
+    console.log('fetch hit');
+fetch(baseURL)
 .then(function(response) {
+  //  console.log(response);
     return response.json();
 })
-.catch(function(myJson) {
-console.log(myJson)
+.then(data => display(data))
+.catch(function(json) {
+console.log(json)
 })
+}
+
+
+function display(cats) {
+    //console.log('display', cats);
+    //console.log(cats[0].text);
+    let fact = cats.text;
+    console.log(fact);
+
+    let footer = document.querySelector('footer')
+    console.log(footer);
+    footer.innerText = fact;
+}
